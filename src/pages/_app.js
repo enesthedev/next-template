9ac4051b6630d29e44621c5928570654de491087
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-06-14 01:40:49 +0300

    Refactor: Formatted all files with prettier/ESLint

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,9 @@
-import 'styles/globals.css'
+import "styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const test = "a";
+  const b = "b";
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
